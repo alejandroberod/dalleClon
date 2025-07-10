aafde021e36e53424ed994b5500a08e01c70452d
Alejandro Betancourth <luisbetancourth43@gmail.com>
2025-07-09 22:34:26 -0500

    Se agrega funcionalidad de buscador

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -7,6 +7,8 @@ export default function Home() {
   const [loading, setLoading] = useState(false);
   const [allPosts, setAllPosts] = useState(null);
   const [searchText, setSearchText] = useState("");
+  const [searchedResults, setSearchedResults] = useState(null);
+  const [searchTimeout, setSearchTimeout] = useState(null);
 
   useEffect(() => {
     async function getPosts() {
@@ -26,6 +28,23 @@ export default function Home() {
     getPosts();
   }, []);
 
+  function handleSearchChange(e) {
+    clearTimeout(searchTimeout);
+    setSearchText(e.target.value);
+
+    setSearchTimeout(
+      setTimeout(() => {
+        const searchResults = allPosts.filter(
+          (item) =>
+            item.name.toLowerCase().includes(searchText.toLowerCase()) ||
+            item.prompt.toLowerCase().includes(searchText.toLowerCase())
+        );
+
+        setSearchedResults(searchResults);
+      }, 500)
+    );
+  }
+
   return (
     <div>
       <section className="max-w-7xl mx-auto">
@@ -40,7 +59,14 @@ export default function Home() {
         </div>
 
         <div className="mt-16">
-          <FormField />
+          <FormField
+            handleChange={handleSearchChange}
+            labelName="Search posts"
+            type="text"
+            name="text"
+            placeholder="Search posts"
+            value={searchText}
+          />
         </div>
 
         <div className="mt-10">
@@ -58,7 +84,7 @@ export default function Home() {
               )}
               <div className="grid lg:grid-cols-4 sm:grid-cols-3 xs:grid-cols-2 grid-cols-1 gap-3">
                 {searchText ? (
-                  <RenderCards data={[]} title="No search results found" />
+                  <RenderCards data={searchedResults} title="No search results found" />
                 ) : (
                   <RenderCards data={allPosts} title="No posts found" />
                 )}
